3e9b06c4f7612b832b719e258f365b2e3dcbade9
iluna007 <146373472+iluna007@users.noreply.github.com>
2024-06-06 18:26:01 +0000

    agregue prueba para vr

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,7 +7,7 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hola Iker!!</h1>
+			<h1>Hola!</h1>
 
 			
 			<div className="alert alert-info">
--- src/front/js/pages/vr.js
+++ src/front/js/pages/vr.js
@@ -6,8 +6,9 @@ export const VR = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="text-center mt-5">
+    <div className="text-center mt-5"style={{height: '100vh'}}>
       <h1></h1>
+      <iframe class="atis-cloud-iframe" src="https://webapp.atis.cloud/prism/share/TditDUlSAQIEfdJEroQPnUtNACe68qZQg1OO6Y5LTiKMJxTgaM?menu=true&tools=true&map=true&autoload=false" width="100%" height="100%"></iframe>
     </div>
   );
 };
